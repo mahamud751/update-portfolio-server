a0cd66086e34b74c1d4c51dcd0b7488a506dd09e
Mahamud Pino <mahamudpino24678@gmail.com>
2022-07-19 00:46:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
-
-require('dotenv').config();
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+
 
 
 const port = process.env.PORT || 5000
